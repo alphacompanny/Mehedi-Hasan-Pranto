20e7cdd5e0cfe1e1d4e982c370943e1afe589398
Alpha Maketing Agency <alphacompany1002@gmail.com>
2026-06-20 01:43:18 +0600

    Add files via upload

---
+++ Area of a Rectangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float length, width, area;
+
+    printf("Enter length of the rectangle: ");
+    scanf("%f", &length);
+
+    printf("Enter width of the rectangle: ");
+    scanf("%f", &width);
+
+    area = length * width;
+    printf("Area of the rectangle is: %.2f\n", area);
+
+    return 0;
+}

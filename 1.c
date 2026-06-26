515565dfff6bbf7076d6784b2944c10b5afcd339
Alpha Maketing Agency <alphacompany1002@gmail.com>
2026-06-26 22:47:09 +0600

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+
+    printf("Enter your number: ");
+    scanf("%d", &a);
+
+    printf("Before: %d\n", a);
+    printf("Using a++: %d\n", a++);
+    printf("After: %d\n", a);
+
+    return 0;
+}
